badcf168b85af07b2cef8da028ece8d16751f12c
JuanRa007 <juanra.hortal@gmail.com>
2023-04-02 12:00:42 +0200

    Avances varios

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicosComponent {
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid && this.miFormulario?.controls['producto']?.touched
+  }
+
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.touched && this.miFormulario?.controls['precio']?.value < 0;
+  }
+
+  /*   guardar(miFormulario: NgForm) { */
+  guardar() {
+    console.log(this.miFormulario);
+  }
 
 }
